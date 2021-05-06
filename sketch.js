17d8e7b4ff8ad10add7c3e816ff0c87d174d563b
Ruta-123 <72984772+Ruta-123@users.noreply.github.com>
2021-05-06 18:10:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var balloon,balloonImage1,balloonImage2;
-// create database and position variable here
+var position,database
 
 function preload(){
    bg =loadImage("cityImage.png");
@@ -17,6 +17,7 @@ function setup() {
   balloon=createSprite(250,450,150,150);
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
+  database.ref("Balloon/Height").on("value",readPosition)
 
   textSize(20); 
 }
@@ -27,19 +28,19 @@ function draw() {
 
   if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in left direction
+    update(-5,0)
   }
   else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in right direction
+    update(+5,0)
   }
   else if(keyDown(UP_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in up direction
+    update(0,-5)
   }
   else if(keyDown(DOWN_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in down direction
+    update(0,+5)
   }
 
   drawSprites();
@@ -47,4 +48,17 @@ function draw() {
   stroke("white");
   textSize(25);
   text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function readPosition(data){
+position=data.val()
+balloon.x=position.x
+balloon.y=position.y 
+}
+
+function update(x,y){
+database.ref("Balloon/Height").set({
+  X:position.x+x,
+  Y:position.y+y
+})
+
 }
